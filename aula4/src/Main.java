2b8cdc17a2544a6284ad95df1c0781955ba45de0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-13 20:53:14 -0300

    feat: Trabalhando com vetores.

---
+++ aula4/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Double> notas= new ArrayList<>();
+
+        notas.add(10.0);
+        notas.add(8.0);
+
+        for(double nota:notas){
+            System.out.println("Notas:"+nota);
+        }
+    }
+}
