08fce64f23c7cf509bbf18d0d69b2ca5e6f0b629
gustavogsam <gustavo.mirales@ba.estudante.senai.br>
2025-09-10 21:37:19 -0300

    spring boot

---
+++ projeto/src/main/java/com/example/projeto/controller/Welcome.java
@@ -0,0 +1,28 @@
+package com.example.projeto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/senai")
+
+public class Welcome {
+
+    @GetMapping ("/")
+
+    public String mensagem(){
+        return "Bem vindo ! ";
+
+    }
+
+    @GetMapping("/dev")
+    public String dev(){
+        return "Gustavo";
+    }
+
+    @GetMapping("/novo")
+    public String novo(){
+        return "Sucesso";
+    }
+}
